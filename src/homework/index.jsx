6703fed1fcf690ffefd547db6ee9de91d56815b4
Pakorn Sangpeth 🔌 <xkamail@icloud.com>
2018-08-12 19:50:49 +0700

    sd

--- src/homework/index.jsx
+++ src/homework/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Title } from '../home';
 import List from './List';
 import { Link } from 'react-router-dom';
+import store from './store';
+
+
+store.submitAddHomework();
 
 const Homework = () => (
   <div>
--- src/homework/store.js
+++ src/homework/store.js
@@ -1,15 +1,29 @@
 import { observable, action } from 'mobx';
+import { database } from '../firebase';
 
 class homeworkStore {
+
   @observable
   homework = {
     items: []
   };
+  
   @observable
   params = {
     page: 1,
     sort: 'asc',
     limit: 10
+  };
+  
+  @observable
+  homework_data = {
+    name: '',
+    description: ''
+  };
+  
+  @action
+  submitAddHomework = async () => {
+    database.ref('homework').push(this.homework_data);
   };
 
   @action
